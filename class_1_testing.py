11fd033898cef379637e8dd2bad353ee6d779a1c
sunenag <sunenagupta@uchicago.edu>
2022-04-06 20:31:11 -0500

    Created class example

--- class_1_testing.py
+++ class_1_testing.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 31 10:29:45 2021
+Spyder Editor
 
-@author: Jeff Levy
+This is a temporary script file.
 """
 
-print('Hello world!')
-
-print('No more math.')
+print('Hello World!')
+x=10
+print(x*2)
